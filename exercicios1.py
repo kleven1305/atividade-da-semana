539dd0cbae5a7b74e8a752c43b78133a25976779
kleven1305 <kleven.royale@gmail.com>
2019-08-13 16:25:36 -0300

    adicionar python

---
+++ exercicios1.py
@@ -0,0 +1,5 @@
+funcionarios =(input('digite seu nome: '))
+qtd_horas= int(input('digite suas horas trabalhadas: '))
+volor_horas= float(input('digite o valor ganho hora de: ' + funcionarios))
+salario = volor_horas * qtd_horas
+print('Salario de' + funcionarios + 'R$' +  str(salario))
